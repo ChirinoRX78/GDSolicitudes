cf9bcc3e914df68179a4a9e2d700cead05e9d231
ChirinoRX78 <chirinowind@outlook.com>
2026-02-09 14:00:03 -0600

    Initial Commit

---
+++ GDSolicitudes/GDSolicitudesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDSolicitudesApp.swift
+//  GDSolicitudes
+//
+//  Created by TI on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GDSolicitudesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
