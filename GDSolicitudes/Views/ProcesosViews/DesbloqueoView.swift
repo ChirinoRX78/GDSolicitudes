53cc02ef646c39b4d7a4a8c3d78e58cf9bb2df24
ChirinoRX78 <chirinowind@outlook.com>
2026-02-16 08:49:38 -0600

    Se agrega botón para visualizar archivos (Aún en proceso)

--- GDSolicitudes/Views/ProcesosViews/DesbloqueoView.swift
+++ GDSolicitudes/Views/ProcesosViews/DesbloqueoView.swift
@@ -49,6 +49,16 @@ struct DesbloqueoView: View {
                         .foregroundColor(.white)
                         .padding(.leading, 20)
                     Spacer()
+                    Button(action: {
+                        //
+                    }) {
+                        Image(systemName: "paperclip")
+                            .foregroundColor(.white)
+                            .padding(.leading, 30)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                    Spacer()
+                        .frame(width: 40)
                 }
                 .navigationBarBackButtonHidden(true)
                 .padding(10)
